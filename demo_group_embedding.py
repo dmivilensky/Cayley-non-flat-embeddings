44fad31799aa35c212496ee3dee407370e083b51
dmivilensky <pasechnyuk2004@gmail.com>
2021-10-06 09:49:17 +0300

    warmup

--- demo_group_embedding.py
+++ demo_group_embedding.py
@@ -7,6 +7,7 @@ import itertools
 from torch.nn.utils.rnn import pack_padded_sequence, pad_sequence
 from torch.utils.data import Dataset, DataLoader
 import Levenshtein as lev
+from lr_scheduler import WarmupMultiStepLR
 
 
 class Encoder(torch.nn.Module):
@@ -19,7 +20,7 @@ class Encoder(torch.nn.Module):
 
     def forward(self, text, text_lengths):
         packed_embedded = pack_padded_sequence(
-            self.embedding(text), text_lengths, 
+            self.embedding(text), text_lengths,
             batch_first=True, enforce_sorted=False
         )
         packed_output, (hidden, cell) = self.lstm(packed_embedded)
@@ -37,13 +38,14 @@ class GroupDataset(Dataset):
 
     def __getitem__(self, idx):
         length = max(1, int(np.random.poisson(lam=5.0)))
-        sequence = [random.choice(list(range(1, 1 + 2 * self.generators))) for _ in range(length)]
+        sequence = [random.choice(
+            list(range(1, 1 + 2 * self.generators))) for _ in range(length)]
         return {'sequence': (sequence, length)}
 
 
 def collate_wrapper(batch):
     return pad_sequence([
-        torch.IntTensor(sen['sequence'][0]) 
+        torch.IntTensor(sen['sequence'][0])
         for sen in batch
     ], batch_first=True, padding_value=0), [sen['sequence'][1] for sen in batch]
 
@@ -53,16 +55,19 @@ def pairwise_distances(sequences):
     result = np.zeros(shape=(batch_size, batch_size))
     for i in range(batch_size):
         for j in range(i + 1):
-            result[i, j] = result[j, i] = lev.distance("".join(map(str, sequences[i])), "".join(map(str, sequences[j])))
+            result[i, j] = result[j, i] = lev.distance(
+                "".join(map(str, sequences[i])), "".join(map(str, sequences[j])))
     return torch.Tensor(result)
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = Encoder().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+scheduler = WarmupMultiStepLR(optimizer, warmup_iters=10)
+
 criterion = torch.nn.MSELoss().to(device)
 
-epochs = 20
+epochs = 40
 batch_size = 10
 steps = 30
 samples_count = 100
@@ -75,8 +80,8 @@ for _ in range(epochs):
 
     model.train()
     data_loader = itertools.islice(DataLoader(
-        group_dataset, batch_size=batch_size, 
-        shuffle=True, num_workers=0, 
+        group_dataset, batch_size=batch_size,
+        shuffle=True, num_workers=0,
         collate_fn=collate_wrapper
     ), batch_size*steps)
 
@@ -86,13 +91,14 @@ for _ in range(epochs):
 
         embeddings = model(sequences, sequences_lengths)
         loss = criterion(
-            torch.cdist(embeddings, embeddings, p=2).unsqueeze(0), 
+            torch.cdist(embeddings, embeddings, p=2).unsqueeze(0),
             pairwise_distances(sequences).unsqueeze(0)
         )
         loss.backward()
         optimizer.step()
+        scheduler.step()
 
         epoch_loss += loss.item()
         iters += 1
-    
+
     print('loss:', epoch_loss / iters)
